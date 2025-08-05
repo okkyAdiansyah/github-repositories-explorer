13b28bec47dfe0bd440c0a32d6ce22fafa710d85
okkyAdiansyah <okkyadiansyah.0610@gmail.com>
2025-08-05 19:24:34 +0700

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://okkyadiansyah.github.io/github-repositories-explorer/',
   plugins: [
     react(),
     tailwindcss()
